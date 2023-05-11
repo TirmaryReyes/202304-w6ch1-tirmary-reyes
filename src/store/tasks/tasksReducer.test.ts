369d9b6ce291eb9e9dd3cb5010c30c4b0467f095
Tirmary Reyes <tmrv1991@gmail.com>
2023-05-11 11:00:54 +0200

    Fix for misrouted import

--- src/store/tasks/tasksReducer.test.ts
+++ src/store/tasks/tasksReducer.test.ts
@@ -5,8 +5,6 @@ import {
   tasksReducer,
 } from "./tasksSlice";
 
-import { TasksStructure } from "../types";
-
 describe("Given a tasksSlice reducer", () => {
   describe("When it receives an empty tasks list and a load action with two task", () => {
     test("Then it should return a tasks list with two tasks", () => {
